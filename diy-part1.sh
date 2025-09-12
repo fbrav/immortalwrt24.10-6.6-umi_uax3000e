7b5cd16133ee49d1e31c4cffc11058ec78df2c4a
fbrav <58653349+fbrav@users.noreply.github.com>
2025-09-13 01:04:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,6 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/util/xml/g' /usr/lib/lua/luci/model/cbi/easytier.lua
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
