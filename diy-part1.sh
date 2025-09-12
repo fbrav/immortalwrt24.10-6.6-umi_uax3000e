cd6b7cc668d727b456550fe880eb8e549fe81768
fbrav <58653349+fbrav@users.noreply.github.com>
2025-09-12 13:39:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 #git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
+git clone https://github.com/EasyTier/luci-app-easytier.git package/easytier
