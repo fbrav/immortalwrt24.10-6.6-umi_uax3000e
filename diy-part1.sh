0890743fe87e0d7d69dbba7586e14e359d1697c2
fbrav530 <58653349+fbrav530@users.noreply.github.com>
2026-07-06 23:12:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@
 #git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 #git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
 git clone https://github.com/EasyTier/luci-app-easytier.git package/easytier
+git clone https://github.com/FUjr/QModem.git package/qmodem
