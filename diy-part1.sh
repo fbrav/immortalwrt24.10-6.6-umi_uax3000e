ccbb3c6ea99879d52991803810fef98232c3452f
fbrav530 <58653349+fbrav530@users.noreply.github.com>
2026-07-07 19:45:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,4 +20,4 @@
 #git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
 git clone https://github.com/EasyTier/luci-app-easytier.git package/easytier
 git clone https://github.com/fbrav530/luci-app-generic-runner.git package/runner
-echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
+#echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
